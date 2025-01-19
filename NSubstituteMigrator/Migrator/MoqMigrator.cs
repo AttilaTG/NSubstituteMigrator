d5c9c5e6ebce885879a9d576ee2d0e7ffa4f5123
AttilaTG <attila.tito.1c@gmail.com>
2025-01-19 22:13:01 +0100

    refactor: add All option to migrate every project

--- NSubstituteMigrator/Migrator/MoqMigrator.cs
+++ NSubstituteMigrator/Migrator/MoqMigrator.cs
@@ -1,37 +1,61 @@
-﻿using NSubstituteMigrator.Migrator.Implementations;
-
-namespace NSubstituteMigrator.Migrator
+﻿namespace NSubstituteMigrator.Migrator
 {
     public class MoqMigrator
     {
         private readonly MigratorService _migratorService;
         public MoqMigrator()
         {
-            _migratorService =  new MigratorService(new DirectoryWrapper(), new XmlLoader());
+            _migratorService =  new MigratorService();
         }
         public void MigrateToNSubstitute() 
         {
-            Console.WriteLine("Selecciona el directorio raíz:");
-            var rootFolder = Console.ReadLine()?.Trim();
-
-            if (string.IsNullOrWhiteSpace(rootFolder) || !Directory.Exists(rootFolder))
+            try
             {
-                Console.WriteLine("Directorio inválido.");
+                Console.WriteLine("Selecciona el directorio que contenga tus proyectos de test:");
+                var rootFolder = Console.ReadLine()?.Trim();
+
+                if (string.IsNullOrWhiteSpace(rootFolder) || !Directory.Exists(rootFolder))
+                {
+                    Console.WriteLine("Directorio inválido.");
+                    return;
+                }
+
+                var testProjects = _migratorService.GetTestProjects(rootFolder);
+
+                if (!testProjects.Any())
+                {
+                    Console.WriteLine("No se encontraron proyectos de test.");
+                    return;
+                }
+
+                var selectedProjectPath = _migratorService.SelectTestProject(testProjects);
+
+                Console.WriteLine($"Has seleccionado: {Path.GetFileNameWithoutExtension(selectedProjectPath)}");
+
+                if (selectedProjectPath.Equals("All"))
+                {
+                    foreach (var project in testProjects)
+                    {
+                        MigrateProject(project);
+                    }
+                }
+                else
+                {
+                    MigrateProject(selectedProjectPath);
+                }
+
+                Console.WriteLine("Migración completada.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
                 return;
             }
+        }
 
-            var testProjects = _migratorService.GetTestProjects(rootFolder);
-
-            if (!testProjects.Any())
-            {
-                Console.WriteLine("No se encontraron proyectos de test.");
-                return;
-            }
-
-            var selectedProjectPath = _migratorService.SelectTestProject(testProjects);
-
-            Console.WriteLine($"Has seleccionado: {Path.GetFileNameWithoutExtension(selectedProjectPath)}");
-
+        #region private methods
+        private void MigrateProject(string selectedProjectPath) 
+        {
             var testFiles = GetMoqFiles(selectedProjectPath);
 
             if (!testFiles.Any())
@@ -43,11 +67,8 @@ namespace NSubstituteMigrator.Migrator
             Console.WriteLine($"Archivos detectados que usan Moq: {testFiles.Count()}");
 
             MigrateFiles(testFiles);
-
-            Console.WriteLine("Migración completada.");
         }
-
-        private void MigrateFiles(IEnumerable<string> testFiles) 
+        private void MigrateFiles(IEnumerable<string> testFiles)
         {
             foreach (var file in testFiles)
             {
@@ -56,11 +77,12 @@ namespace NSubstituteMigrator.Migrator
             }
         }
 
-        private IEnumerable<string> GetMoqFiles(string selectedProjectPath) 
+        private IEnumerable<string> GetMoqFiles(string selectedProjectPath)
         {
             return Directory.GetFiles(Path.GetDirectoryName(selectedProjectPath)!, "*.cs", SearchOption.AllDirectories)
                                      .Where(file => File.ReadAllText(file).Contains("using Moq"))
                                      .ToList();
         }
+        #endregion
     }
 }
